1b884d70eec284937976e9b7cb7e7948737ef410
Gilles Migliori <gilles.migliori@gmail.com>
2018-04-27 06:53:31 +0200

    hoek vulnerability issue

--- class/bootstrap/nav/Nav.php
+++ class/bootstrap/nav/Nav.php
@@ -25,12 +25,16 @@ class Nav
         if ($active === true) {
             $link_attr = Utils::addClass($this->active_link_class, $link_attr);
         }
+        // wrap text with span if it doesn't begin/finish with a tag
+        if (!preg_match('`^<(.*)+>`', trim($text))) {
+            $text = '<span class="sidenav-text">' . $text . '</span>';
+        }
         $dropdown = '';
         if (!empty($dropdown_id)) {
             // create subnav if any
             $this->$dropdown_obj_id = new Nav($dropdown_id, $dropdown_class);
             $dropdown               = $this->$dropdown_obj_id;
-            $link_attr              = Utils::addClass('dropdown-toggle', $link_attr);
+            $link_attr              = Utils::addClass('dropdown-toggle collapsed', $link_attr);
         }
         $this->items[] = array(
             'url'            => $url,
